cf6298c4d579653fdb57118872c77c93fa377c14
Darien Miranda <panzerfausten@gmail.com>
2015-05-05 11:08:30 -0700

    fixed ticker

--- server/static/timerWorker.js
+++ server/static/timerWorker.js
@@ -1,13 +1,8 @@
 var i = 0;
 var max_time = 60; //secs
-var tick_time = 15 //Time in seconds left to start the ticking sound
 function timedCount() {
     i = i + 1;
     postMessage(max_time - i);
-    if(i < tick_time +1){
- 
-
-    }
     setTimeout("timedCount()",1000);
 }
 
